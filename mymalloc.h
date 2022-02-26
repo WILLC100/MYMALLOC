a8ad82de07a96b0311542b34fa45b4eaba3f7d08
William H. Chen <whc27@ilab1.cs.rutgers.edu>
2022-02-26 16:53:23 -0500

    base

--- mymalloc.h
+++ mymalloc.h
@@ -1,3 +1,12 @@
-#include <stdlib.h>
-#include <stdio.h>
 
+#ifndef _MYMALLOC_H_
+#define _MYMALLOC_H_ 
+
+
+
+
+void* mymalloc(size_t size, char* file, int line);
+
+void free(void* pointer, char* file, int line); 
+
+#endif
