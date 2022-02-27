c1cd1ca7d8768c37c64aa9f22a6621c9f4baad59
William H. Chen <whc27@ilab1.cs.rutgers.edu>
2022-02-27 13:59:06 -0500

    one

--- memgrind.c
+++ memgrind.c
@@ -1,4 +1,13 @@
-
-#include <stdlib.h>
-#include <stdio.h>
+ 
 #include "mymalloc.h"
+
+
+
+int main(int argc, char* argv[] ){ // runs program here 
+
+    //must initialize first metadata node with byte manipulation 
+
+    
+
+
+}
