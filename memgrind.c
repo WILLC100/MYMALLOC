b0b4d86dff0ad3a0c91cce23be1e57849bcac2bf
William H. Chen <whc27@ilab1.cs.rutgers.edu>
2022-02-27 14:21:40 -0500

    one

--- memgrind.c
+++ memgrind.c
@@ -1,4 +1,6 @@
- 
+     
+#include <stdlib.h>
+#include <stdio.h>
 #include "mymalloc.h"
 
 
@@ -7,7 +9,14 @@ int main(int argc, char* argv[] ){ // runs program here
 
     //must initialize first metadata node with byte manipulation 
 
-    
+     FILE* fp = fopen(argv[1], "r");
+    if (!fp) {
+        perror("File cannot be opened.");
+        exit(EXIT_FAILURE);
+    }
 
+
+    int wack = 1; 
+    wack = wack +2;
 
 }
