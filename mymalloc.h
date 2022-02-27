bedb0335b850ac1c33f1bd17340d00b50f5b3f40
William H. Chen <whc27@ilab1.cs.rutgers.edu>
2022-02-27 14:05:36 -0500

    one

--- mymalloc.h
+++ mymalloc.h
@@ -3,10 +3,21 @@
 #define _MYMALLOC_H_ 
 
 
+typedef struct node {
 
+    unsigned char isfree;    // 0 for false, 1 for true 
+    unsigned int blocksize; //size of requested block 
+    struct node* nextblock; // points to next metadata node; 
+    void* blocklocation;    //address of this node's block 
+
+} metadata;
 
 void* mymalloc(size_t size, char* file, int line);
 
-void free(void* pointer, char* file, int line); 
+void myfree(void* pointer, char* file, int line); 
 
+
+#define malloc(s) mymalloc(s, __FILE__, __LINE__)
+#define free(p) myfree(p, __FILE__, __LINE__)
+ 
 #endif
