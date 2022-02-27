8c2c9b69b5efda681cf28f47d8460c67aa494ed6
William H. Chen <whc27@ilab1.cs.rutgers.edu>
2022-02-27 13:59:21 -0500

    one

--- mymalloc.c
+++ mymalloc.c
@@ -9,13 +9,47 @@ static char memory[MEMSIZE];
 
 typedef struct node {
 
-    unsigned int isfree;    // 0 for false, 1 for true 
+    unsigned char isfree;    // 0 for false, 1 for true 
     unsigned int blocksize; //size of requested block 
     struct node* nextblock; // points to next metadata node; 
     void* blocklocation;    //address of this node's block 
 
 } metadata;
 
+void* malloc(size_t size ){
+
+    
+    unsigned int requestedblocksize = size; //holds the requested block size
+  
+    
+ 
+
+    if(!memory[0]){ //first byte check. if 0: first block is free create allocation immediately, else create data structure
 
 
+        metadata* first = &(memory[0]);
 
+        memory[0] = 1; 
+        first->blocksize = requestedblocksize; 
+        first->nextblock = NULL;
+        first->blocklocation = &(memory[13]); 
+        
+
+        
+
+    }else{
+
+
+    }
+     
+    
+
+}
+
+void free(void* pointer ){
+
+
+}
+
+
+ 
