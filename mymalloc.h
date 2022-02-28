db7dc472378a5c4cbcad1a73518a4aefca0ced00
William H. Chen <whc27@ilab2.cs.rutgers.edu>
2022-02-27 22:15:48 -0500

    one

--- mymalloc.h
+++ mymalloc.h
@@ -3,12 +3,16 @@
 #define _MYMALLOC_H_ 
 
 
-typedef struct node { //this node type holds the metadata of each block. 
+typedef struct node { //this node type holds the metadata of each block. size of 24. 
 
     unsigned char istaken;   // 0 for false, 1 for true 
+    unsigned char buffer1; 
+    unsigned char buffer2;
+    unsigned char buffer3;
     unsigned int blocksize; //size of requested block 
     struct node* next; //points to next metadata node; 
     void* blocklocation;    //address of this node's block 
+  
 
 } metadata;
 
