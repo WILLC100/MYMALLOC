56d9acafcefddcf14d222ab713fd0dc5a7807589
William H. Chen <whc27@ilab4.cs.rutgers.edu>
2022-02-28 13:34:30 -0500

    one

--- memgrind.c
+++ memgrind.c
@@ -1,6 +1,7 @@
      
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
 #include "mymalloc.h"
 
 
@@ -14,7 +15,7 @@ int main(int argc, char* argv[] ){ // runs program here
         perror("File cannot be opened.");
         exit(EXIT_FAILURE);
     }
-
+/*
     unsigned int count;
     fscanf(fp, "%d", &count);
     char* test1 =NULL;
@@ -31,26 +32,43 @@ int main(int argc, char* argv[] ){ // runs program here
         
         printf("%p ", test1arr[i]);
 
-        if((i+1)%6 == 0){
-
-           
+        if((i+1)%6 == 0){         
             printf("Line %d", wack);
             printf("\n");
             wack = wack+1;
         }
-
-         
-      
-
     }
     for(int i=0; i<count;i++){
         free(test1arr[i]);
+    }
+    free(test1arr);
+ */
+
+    //test 1
+
+    int* ptr[1000];
+    struct timeval startTime; 
+    struct timeval endTime; 
+    float totalTimeB = 0;
+    for(int i =0; i<100; i++){
+        gettimeofday(&startTime, NULL);
+        for(int j=0; j<120; j++){
+            
+            ptr[j] = (int*)malloc(1);
+            free(ptr[j]);
+            ptr[j] = NULL;
+        }
+        gettimeofday(&endTime, NULL);
+        //printf("%ld\n", endTime.tv_sec);
+        totalTimeB = totalTimeB + ((endTime.tv_sec - startTime.tv_sec)*1000000
+         + (endTime.tv_usec - startTime.tv_usec));
 
     }
-     
-    free(test1arr);
- 
+    printf("The mean time to execute Test 1 %f microseconds\n", totalTimeB/100);
+
 
     fclose(fp);
+
+    return 0; 
 
 }
