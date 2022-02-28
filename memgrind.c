7488c622c5a8423373c760f561b5c48248193663
William H. Chen <whc27@ilab2.cs.rutgers.edu>
2022-02-27 23:56:12 -0500

    one

--- memgrind.c
+++ memgrind.c
@@ -15,8 +15,42 @@ int main(int argc, char* argv[] ){ // runs program here
         exit(EXIT_FAILURE);
     }
 
+    unsigned int count;
+    fscanf(fp, "%d", &count);
+    char* test1 =NULL;
+    
+    unsigned int wack = 0;
 
-    int wack = 1; 
-    wack = wack +2;
+    char** test1arr = malloc(sizeof(char*)*count);
+
+    for(int i =0; i<count; i++ ){
+
+        test1 = malloc(1);
+
+        test1arr[i] = test1;
+        
+        printf("%p ", test1arr[i]);
+
+        if((i+1)%6 == 0){
+
+           
+            printf("Line %d", wack);
+            printf("\n");
+            wack = wack+1;
+        }
+
+         
+      
+
+    }
+    for(int i=0; i<count;i++){
+        free(test1arr[i]);
+
+    }
+     
+    free(test1arr);
+ 
+
+    fclose(fp);
 
 }
