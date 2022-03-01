67a0c5e470257473fe79d62108adfcec1b3b31e5
William H. Chen <whc27@ilab4.cs.rutgers.edu>
2022-03-01 00:43:04 -0500

    one

--- memgrind.c
+++ memgrind.c
@@ -1,28 +1,29 @@
-     
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
-#include "mymalloc.h"
+#include <time.h>
 
-
-
-int main(int argc, char* argv[] ){ // runs program here 
-
-    int *ptr[1000];
+int main(int argc, char* argv[])
+{   
+    unsigned int arraysize = 256;
+    int *ptr[arraysize];
     struct timeval startTime;
     struct timeval endTime;
     float totalTimeOne = 0;
     float totalTimeTwo = 0;
-   // int totalTimeThree = 0;
-   // int totalTimeFour = 0;
+    float totalTimeThree = 0;
+    float totalTimeFour = 0;
+    float totalTimeFive = 0;
+
+    for(int i =0;i<arraysize;i++){
+        ptr[i] = NULL;
+    }
     
     //Test1
     for(int i = 0; i < 50; i++){
         gettimeofday(&startTime, 0);
         for(int j = 0; j < 120; j++){
-            ptr[j] = (int*)(malloc(1));
-          
-           
+            ptr[j] = (int *) malloc(1);
             free(ptr[j]);
             ptr[j] = NULL;
         }
@@ -30,7 +31,7 @@ int main(int argc, char* argv[] ){ // runs program here
         totalTimeOne = totalTimeOne + ((endTime.tv_sec-startTime.tv_sec)*1000000 
         + (endTime.tv_usec-startTime.tv_usec));
     }
-
+    
     printf("The average time to execute Test 1 %f milliseconds\n", totalTimeOne/50);
     
     //Test2
@@ -38,21 +39,160 @@ int main(int argc, char* argv[] ){ // runs program here
         gettimeofday(&startTime, 0);
         for(int j = 0; j < 120; j++){
             ptr[j] = (int *) malloc(1);
-          //  printf("%d %p \n", j, ptr[j]); last allocation from 76 onwards DNE
+           // printf("Malloc %d\n", j);
         }
-        
-        for(int g = 0; g < 120; g++){
-         //   printf("%d %p \n", i, ptr[g]); // round 84, test 76. 
-            free(ptr[g]);
-            ptr[g] = NULL;
+        for(int j = 0; j < 120; j++){
+            free(ptr[j]);
+           // printf("Free %d\n", j);
+            ptr[j] = NULL;
         }
+        //printf("%d", i);
         gettimeofday(&endTime, 0);
         totalTimeTwo = totalTimeTwo + ((endTime.tv_sec-startTime.tv_sec)*1000000
         + (endTime.tv_usec-startTime.tv_usec));
     }
-    
-  
-    printf("The average time to execute Test 2 %f milliseconds\n", totalTimeTwo/50);
 
-    return EXIT_SUCCESS;
+     printf("The average time to execute Test 2 %f milliseconds\n", totalTimeTwo/50);
+    
+    //Test3
+
+    for(int i = 0; i < 50; i++){
+        gettimeofday(&startTime, 0);
+        int current = 0; 
+        
+        
+        srand(time(NULL));
+        
+        while(current < 120){
+            int randomNumber = rand() % 2;
+            // malloc
+            if(randomNumber == 0){
+                ptr[current] = (int*) malloc(1);
+
+               // printf("Malloc %d\n", current);
+                current++;
+            }
+            // free
+            else{
+                
+               for(int i =0; i<arraysize; i++){
+
+                   if(ptr[i] != NULL){
+                      // printf("Free %d \n",i);
+                        free(ptr[i]);
+                        ptr[i] = NULL;  
+                        break;
+                   }
+               }                                
+            }          
+        }
+        gettimeofday(&endTime, 0);
+        totalTimeThree = totalTimeThree + ((endTime.tv_sec-startTime.tv_sec)*1000000
+        + (endTime.tv_usec-startTime.tv_usec));
+        
+        for(int i =0; i<arraysize;i++){ //free the whole array 
+        if(ptr[i] != NULL){
+         //   printf("%d\n", i);
+            free(ptr[i]);
+            ptr[i] = NULL;
+        }
+        
+        
+    }
+    }
+    
+ 
+
+
+
+     printf("The average time to execute Test 3 %f milliseconds\n", totalTimeThree/50);
+    
+    // Test4
+    for(int i = 0; i < 50; i++){
+        gettimeofday(&startTime, 0);
+        for(int j = 0; j < arraysize; j++){
+            ptr[j] = malloc(rand()%3+1);
+           // printf("Malloc %p\n", ptr[j]);
+        }
+         
+        gettimeofday(&endTime, 0);
+
+        totalTimeFour = totalTimeFour + ((endTime.tv_sec-startTime.tv_sec)*1000000
+        + (endTime.tv_usec-startTime.tv_usec));
+
+        for(int i =0; i<arraysize;i++){
+
+        if(ptr[i] != NULL){
+            free(ptr[i]);
+            ptr[i] = NULL;
+        }
+        
+        }
+    }
+    
+    printf("The average time to execute Test 4 %f milliseconds\n", totalTimeFour/50);
+
+    
+    printf("%p\n", &memory[0]);
+    
+
+    // Test5
+
+     
+    unsigned int canmalloc = 1; // true 
+    for(int i = 0; i < 50; i++){
+        gettimeofday(&startTime, 0);
+        
+        int current = 1;
+        int mallocIndex = 0;
+        int freeIndex = 0;
+        
+        while(canmalloc != 0){
+
+            if((ptr[mallocIndex] = malloc(50))){
+                canmalloc = 0;
+                continue;
+            }
+
+            printf("current %d\n", current);
+            current++;
+            mallocIndex++;
+        }
+            
+        for(freeIndex = 0; freeIndex < mallocIndex; freeIndex+=2){
+            printf("freeIndex %d", freeIndex);
+            free(ptr[freeIndex]); 
+            ptr[mallocIndex] = NULL;
+        }
+        for(mallocIndex = 0; mallocIndex < current-1; current+=2){
+            ptr[mallocIndex] = malloc(35);
+        }
+        for(freeIndex = 0; freeIndex < current-1; freeIndex+=1){
+            free(ptr[freeIndex]);
+            ptr[freeIndex] = NULL;
+        }
+        gettimeofday(&endTime, 0);
+        totalTimeFive = totalTimeFive + ((endTime.tv_sec-startTime.tv_sec)*1000000
+        + (endTime.tv_usec-startTime.tv_usec));
+
+        for(int i =0; i<arraysize;i++){
+
+        if(ptr[i] != NULL){
+            free(ptr[i]);
+            ptr[i] = NULL;
+        }
+        
+    }
+    }
+    
+
+    
+    
+    printf("The average time to execute Test 5 %f milliseconds\n", totalTimeFive/50);
+
+    
+
+
+
+    return 0;
 }
